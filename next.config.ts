5062fc5c5c7ced7cfe35620968d5d73c2b2d291b
Sagar Kumar <pandeysagar7991@gmail.com>
2025-07-27 05:51:08 +0530

    Updated something

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // 👈 This is required for static export now
+  images: {
+    unoptimized: true, // 👈 Required if using images in static export
+  },
   experimental: {
     serverActions: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
